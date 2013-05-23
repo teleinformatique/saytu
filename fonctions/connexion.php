4d320c61e83f9e8c5e6a521dd090bf4cc58ebba0
Dodji Gbolohoe <p.midod@gmail.com>
2013-05-23 22:15:15 +0000

    Saytu [fixed]

---
+++ fonctions/connexion.php
@@ -0,0 +1,9 @@
+<?php			try{
+				$bdd = new PDO('mysql:host=localhost;dbname=saytu','root','');
+			}
+			catch(Exception $e){
+				echo "Erreur".$e->getMessage();
+				
+			}
+			
+?>
---
+++ fonctions/saytuSearch.php
@@ -0,0 +1,106 @@
+<?php 
+/*
+*@author : Mouhamadou SEYE
+*@description : Le moteur de recherche
+*/
+// database settings 
+$Host = 'localhost'; 
+$User = 'root'; 
+$Pass = ''; 
+$Base = 'saytu'; 
+
+// mysql table : 
+$Table = 'objet'; 
+// field used for search 
+$champ = 'details'; 
+// number of chars to display 
+$NB_carac = 100; 
+?> 
+<form method="post" action="saytuSearch.php"> 
+<input type="text" name="Psearch"><br /> 
+<input type="submit" value="Search"> 
+</form> 
+<br /> 
+
+<?php 
+if(isset($_POST['Psearch']) && !empty($_POST['Psearch'])) { 
+
+    mysql_connect($Host, $User, $Pass); 
+    mysql_select_db($Base); 
+
+    $search = trim($_POST['Psearch']); 
+    $T = explode(' ', $search); 
+    $count = count($T); 
+
+    // build sql request 
+    if($count > 1) { 
+      $s = " details LIKE '%$search%'
+			OR nomTitulaire LIKE '%$search%'
+			OR prenomTitulaire LIKE '%$search%'
+			OR nomMereTitulaire LIKE '%$search%'
+			OR prenomMereTitulaire LIKE '%$search%'
+			OR libelle LIKE '%$search%'"; 
+      for($i=0; $i<$count; $i++) { 
+          if(empty($T[$i])) continue; 
+        $s .= " OR details LIKE '%$T[$i]%'
+				OR nomTitulaire LIKE '%$T[$i]%'
+				OR prenomTitulaire LIKE '%$T[$i]%'
+				OR nomMereTitulaire LIKE '%$T[$i]%'
+				OR prenomMereTitulaire LIKE '%$T[$i]%'
+				OR libelle LIKE '%$T[$i]%'"; 
+        $Ns[] = $T[$i]; 
+        } 
+      } 
+    else { 
+      $s = " details LIKE '%$search%' 
+			OR nomTitulaire LIKE '%$search%'
+			OR prenomTitulaire LIKE '%$search%'
+			OR nomMereTitulaire LIKE '%$search%'
+			OR prenomMereTitulaire LIKE '%$search%'
+			OR libelle LIKE '%$search%' "; 
+      $Ns[] = $search; 
+      } 
+    $Requete = "SELECT details from $Table WHERE $s"; 
+    $Env = mysql_query($Requete) or die (mysql_error()); 
+
+    if(mysql_num_rows($Env) == 0) $Error = 'No results'; 
+    else $Error = FALSE; 
+    echo $Error; 
+
+    echo '<table border="1">'; 
+    $i = 1; 
+    while ($O = mysql_fetch_object($Env)) { 
+      echo '<tr><td>'; 
+      echo "<u>Results $i</u> <br /><br />"; 
+
+      $count = count($Ns); 
+      if($count > 1) { 
+        for($x=0; $x<$count; $x++) { 
+          if(empty($Ns[$x])) continue; 
+          if(strpos(strtolower($O->$champ),strtolower($Ns[$x]))==TRUE) { 
+            $search1 = $T[$x]; 
+            continue; 
+            } 
+          } 
+        } 
+
+        else $search1 = $Ns[0]; 
+        $pos = strpos(strtolower($O->$champ) , strtolower($search1)); 
+        $O->$champ = substr($O->$champ, $pos, $NB_carac); 
+        $last_space = strrpos($O->$champ, ' '); 
+        $O->$champ = substr($O->$champ, 0, $last_space).' ...'; 
+        $O->$champ = htmlspecialchars($O->$champ); 
+        $O->$champ = nl2br($O->$champ); 
+        $txt = preg_replace('`('.join('|', $Ns).')`i', 
+                            '<b>$1</b>', 
+                            $O->$champ); 
+        $i++; 
+        echo $txt; 
+
+        echo '</td></tr>'; 
+        } 
+	mysql_close();
+    echo '</table><br />'; 
+    } 
+	
+?> 
